6720fc92d274452941d4ed7294f31e8ee71f0d10
Zander2895 <jacquesalexander71@gmail.com>
2024-04-29 21:10:00 -0400

    new commit

--- advanced_list_methods.py
+++ advanced_list_methods.py
@@ -1,7 +1,12 @@
 #Task 1 Find out wich students both submitted their assignments and attended the class 
 submitted = ["Alice", "Bob", "Charlie", "David"]
 attended = ["Charlie", "Eve", "Alice", "Frank"]
-print(submitted == attended)
+
+completed_both = []
+for student in submitted:
+    if student not in attended:
+        completed_both.append(student)
+print(f'This is my matching list: {completed_both}')
 
 #Task 2 Check if the two lists are identical in terms of their content, regardless of order.
 
--- python_list_transformation.py
+++ python_list_transformation.py
@@ -1,17 +1,17 @@
 #Task 1 Sort the list of grades in descending order and display the list.
 grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
-grades.sort(reverse=True)
-print(grades)
+grades.sort(reverse = True)
+print(f'This is my sorted list: {grades}')
 
 #Task 2 Calculate the average grade and display it.
 grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
 total = sum(grades)
 average = total / len(grades)
-print(average)
+print(f'This is my average: {average}')
 
 #Task 3 Replace and grades below 80 with the value "Failed."
 grades = [85, 90, 78, 88, 76, 95, 89, 80, 72, 93]
-grades[2] = "Failed"
-grades[4] = "Failed"
-grades[8] = "Failed"
-print(grades)
+for i in range(len(grades)):
+    if grades[i] < 80:
+        grades[i] = "Failed. "
+print(f'This is my replaced list: {grades}')
